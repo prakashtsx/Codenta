f39d8510869526663179853f40284a75c34f6cc3
prakashtsx <urprakashgupta@gmail.com>
2026-06-19 02:38:49 +0530

    feat: implement authentication flow with requireAuth and requireUnAuth functions, add Dashboard and ProtectedLayout components

--- features/auth/actions/index.ts
+++ features/auth/actions/index.ts
@@ -3,14 +3,22 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import {
+  DEFAULT_AUTH_CALLBACK,
+  getSafeCallbackPath,
+  SIGN_IN_PATH,
+} from "../utils";
 
 export async function signInWithGithub(formData: FormData) {
   const callback = formData.get("callbackUrl");
 
+  const redirectTo = getSafeCallbackPath(
+    typeof callback === "string" ? callback : null,
+  );
   const result = await auth.api.signInSocial({
     body: {
       provider: "github",
-      callbackURL: "/dashboard",
+      callbackURL: redirectTo,
     },
     headers: await headers(),
   });
@@ -24,4 +32,25 @@ export async function getServerSession() {
   return auth.api.getSession({
     headers: await headers(),
   });
+}
+
+export async function requireAuth(redirectTo = SIGN_IN_PATH) {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect(redirectTo);
+  }
+
+  return session;
+}
+
+export default async function requireUnAuth(
+  redirectTo = DEFAULT_AUTH_CALLBACK,
+) {
+  const session = await getServerSession();
+  if (session) {
+    redirect(redirectTo);
+  }
+
+  return session;
 }
